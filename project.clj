9f12f03cd5871e42afd0a7edd33152032c446b67
Isaac Ballone <isaac@ballone.dev>
2023-12-08 13:45:09 -0600

    Auto rebuild codox

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject com.balloneij/familiar-codox-theme "0.1.0"
-  :description "A Javadoc inspired Codox theme"
-  :url "https://github.com/balloneij/familiar-codox-theme"
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/MIT"})
